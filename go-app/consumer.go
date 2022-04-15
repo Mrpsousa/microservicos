b4ed1ad1fb209d7ded2235a48730bc085688e8d7
Mrpsousa <urameshi.uba@gmail.com>
2022-04-15 17:58:12 -0300

    feature: consumer and producer - python - working

--- go-app/consumer.go
+++ go-app/consumer.go
@@ -1,64 +1,30 @@
 package main
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
-func consume(ctx context.Context) {
-	const (
-		topic          = "teste"
-		broker1Address = "localhost:9092"
-	)
-	// initialize a new reader with the brokers and topic
-	// the groupID identifies the consumer and prevents
-	// it from receiving duplicate messages
-	r := kafka.NewReader(kafka.ReaderConfig{
-		Brokers: []string{broker1Address},
-		Topic:   topic,
-	})
-	for {
-		// the `ReadMessage` method blocks until we receive the next event
-		msg, err := r.ReadMessage(ctx)
-		if err != nil {
-			panic("could not read message " + err.Error())
-		}
-		// after receiving the message, log its value
-		fmt.Println("received: ", string(msg.Value))
-	}
-}
-
 func main() {
 
-	// create a new context
-	ctx := context.Background()
-	// produce messages in a new go routine, since
-	// both the produce and consume functions are
-	// blocking
-	consume(ctx)
+	configMap := &kafka.ConfigMap{
+		"bootstrap.servers": "kafka_kafka_1:9092",
+		"topic":             "my-topic-three"}
+
+	c, err := kafka.NewConsumer(configMap)
+
+	if err != nil {
+		fmt.Println("Error consumer", err.Error())
+	}
+
+	topics := []string{"teste"}
+
+	c.SubscribeTopics(topics, nil)
+	for {
+		msg, err := c.ReadMessage(-1)
+		if err == nil {
+			fmt.Println(string(msg.Value), msg.TopicPartition)
+		}
+	}
 }
-
-// configMap := &kafka.ConfigMap{
-// 	"bootstrap.servers": "kafka_kafka_1:9092",
-// 	"client.id":         "gokafka-consumer",
-// 	"group.id":          "gokafka-group",
-// 	"auto.offset.reset": "earliest",
-// }
-// c, err := kafka.NewConsumer(configMap)
-
-// if err != nil {
-// 	fmt.Println("Error consumer", err.Error())
-// }
-
-// topics := []string{"teste"}
-
-// c.SubscribeTopics(topics, nil)
-// for {
-// 	msg, err := c.ReadMessage(-1)
-// 	if err == nil {
-// 		fmt.Println(string(msg.Value), msg.TopicPartition)
-// 	}
-// }
-// }
--- python-app/services/service.py
+++ python-app/services/service.py
@@ -1,6 +1,6 @@
 
 from time import sleep
-
+from json import dumps
 from kafka import KafkaProducer, KafkaConsumer
 
 bootstrap_servers = ['localhost:9091', 'localhost:9092']
@@ -8,28 +8,30 @@ topicName = 'my-topic-three'
 
 
 def run_producer():
-    producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
-    producer = KafkaProducer()
-
-    for e in range(1000):
-        # data = f'number {e}'
-        try:
-            producer.send(topicName, b'Algo Aew!')
-            producer.flush()
-        except Exception as e:
-            print(e)
-            raise
-        sleep(5)
-        print(f"send data number: {e+1}")
-
-
-def run_consumer():
     try:
-        consumer = KafkaConsumer(topicName, bootstrap_servers=bootstrap_servers)
-        print('foi')
+        producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+        producer = KafkaProducer()
+
+        for e in range(1000):
+            data = f"value : {e}"    
+            producer.send(topicName, value=dumps(data).encode('ascii'))
+            producer.flush()
+            print(f"send data number: {e+1}")
+
+            sleep(5)
     except Exception as e:
         print(e)
         raise
-    print(consumer)
+    
+
+def run_consumer():
+    try:
+        consumer = KafkaConsumer('my-topic-three',
+                                 bootstrap_servers=['localhost:9091'],
+                                 auto_offset_reset='earliest',
+                                 enable_auto_commit=True)
+    except Exception as e:
+        print(e)
+        raise
     for message in consumer:
-        print(message)
+        print(message.value)
